161ac91ea902594e64eafc2974f7ec9840a0651c
Jourloy <jourloy@yandex.ru>
2021-07-14 22:02:25 +0300

    Create index.d.ts

---
+++ src/types/index.d.ts
@@ -0,0 +1,20 @@
+/* <----------- GENERAL -----------> */
+
+interface Console {
+    _err: (text: string) => void;
+    _warn: (text: string) => void;
+    _log: (text: string) => void;
+}
+
+/* <----------- SCREEPS -----------> */
+
+interface Creep {
+    _say: (text: string, bool: boolean) => void;
+}
+
+interface CreepMemory {
+    state?: {
+        empty: boolean,
+        busy: boolean,
+    }
+}
